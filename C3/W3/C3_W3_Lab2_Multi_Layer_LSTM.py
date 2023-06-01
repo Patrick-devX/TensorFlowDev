42878e2657a99211fa2e81bc4a9651da06b0b7a4
ptchuente_ <p.tchuente@food.family>
2023-06-01 13:37:36 +0200

    C3 W3: Lab1 imdb rewies subword MULTI LSTM layer

--- C3/W3/C3_W3_Lab2_Multi_Layer_LSTM.py
+++ C3/W3/C3_W3_Lab2_Multi_Layer_LSTM.py
@@ -1,9 +1,10 @@
-# TEXT CLASSIFICATION-Single Layer LSTM- DAtaset pretokenized IMDB Reviews dataset
+# TEXT CLASSIFICATION-MULTI Layer LSTM- DAtaset pretokenized IMDB Reviews dataset
 
 import tensorflow_datasets as tfds
 from tensorflow import keras
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 #Download the subword encoded pretokenized dataset
 dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
@@ -25,18 +26,37 @@ train_dataset = train_data.shuffle(BUFFER_SIZE)
 train_dataset = train_dataset.padded_batch(BATCH_SIZE)
 test_dataset = test_data.padded_batch(BATCH_SIZE)
 
+batch_size = 1
+timesteps = 20
+features = 16
+lstm_dim = 8
 
+# Define array input with random values
+random_input = np.random.rand(batch_size, timesteps, features)
+print(f'shape of input array: {random_input.shape}')
+
+#Define LSTM tha return a sequence
+lstm_rs1 = keras.layers.LSTM(lstm_dim, return_sequences=False)
+result_rsf = lstm_rs1(random_input)
+print(f'shape of LSTM output with return_sequence False {result_rsf.shape}')
+
+#Define LSTM tha return a sequence
+lstm_rs2 = keras.layers.LSTM(lstm_dim, return_sequences=True)
+result_rst = lstm_rs2(random_input)
+print(f'shape of LSTM output with return_sequence True {result_rst.shape}')
 
 ##### Build and compile the model #####
 #Hyperparameter
 embedding_dim = 64
-lstm_dim = 64
+lstm1_dim = 64
+lstm2_dim = 32
 dense_dim = 64
 NUM_EPOCHS = 10
 
 model = keras.Sequential([
     keras.layers.Embedding(tokenizer.vocab_size, embedding_dim, input_length=BATCH_SIZE),
-    keras.layers.Bidirectional(keras.layers.LSTM(lstm_dim)),
+    keras.layers.Bidirectional(keras.layers.LSTM(lstm1_dim, return_sequences=True)),
+    keras.layers.Bidirectional(keras.layers.LSTM(lstm1_dim)),
     keras.layers.Dense(dense_dim, activation='relu'),
     keras.layers.Dense(1, activation='sigmoid')
 ])
